a6eda778d751a5d88d3199ba82d9e6fc08df49f3
Siyanda-Mzam <dot.netntoz@gmail.com>
2017-05-04 19:49:59 +0200

    Refactoring in progress

--- server/app.js
+++ server/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const fs = require('fs');
+const constants = require('./config/constants.json');
+const Product = require('./models/Product.Model.js');
 let mongo = require('mongod'),
     mongoose = require('mongoose'),
-    options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } },
-    db = mongoose.connect("mongodb://simzam:7bitsfacebook7@ds131151.mlab.com:31151/products", options),
-    Product = require('./models/Product.Model.js');
-mongoose.Promise = global.Promise;
+    db = mongoose.connect(constants.DB.ORIGIN + "://" +
+                          constants.DB.USER + ":" +
+                          constants.DB.PASS + "@" +
+                          constants.DB.DOMAIN + ":" +
+                          constants.DB.PORT + "/" +
+                          constants.DB.COLLECTION,
+                          constants.DB.CONNECTION_OPTS);
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log("Sucessfully connectected to database");
 });
 const app = express();
 
-app.set('port', (process.env.PORT || 4300));
+app.set('port', (process.env.PORT || constants.SERVER.PORT));
 
 //Only server static files in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === constants.ENV.PROD) {
   app.use(express.static('src/build'));
 }
 
