863dee2996e5b2bd7b7d7f53030363d0a94fbe35
Siyanda Mzam <siyanda@fcb.co.za>
2017-06-05 11:22:34 +0200

    Fixed the default connection to database. Now only connecting on request. Fixed mongoose promise issue.

--- server/app.js
+++ server/app.js
@@ -7,16 +7,9 @@ let feathers = require('feathers'),
     Product = require('./models/Product.Model.js'),
     mongo = require('mongod'),
     mongoose = require('mongoose'),
-    storeList = [],
-    db = mongoose.connect(config.DB.CONNECTION_STRING + config.DB.DEFAULT_DB, config.DB.CONNECTION_OPTS);
+    storeList = [];
 
 mongoose.Promise = global.Promise;
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log("Sucessfully connectected to database");
-});
-
 const app = feathers()
   .configure(rest())
   .use(bodyParser.json())
@@ -28,41 +21,41 @@ if (process.env.NODE_ENV === config.ENV.PROD) {
   app.use(express.static('src/build'));
 }
 
-exist = (store) => {
-  if (store !== undefined && store !== null) {
-    if (storeList.includes(store)) {
-      return true;
-    }
-  }
-  return false;
-}
-
 app.get('/api/find-store', (req, res) => {
   const store = req.query.q.toLowerCase();
-  if (!exist(store)) {
-    storeList.push(store);
-  }
+  storeList = [];
+  storeList.push(store);
   console.log("Stores pushed: " + storeList);
+  reconnectToDB(store)
+  return res.json(req.query.q);
+});
+
+reconnectToDB = (store) => {
+  let message = '';
   db = mongoose.disconnect().then(function() {
     switch (store) {
       case 'truworths':
         db = mongoose.connect(config.DB.CONNECTION_STRING + config.DB.TRUWORTHS_DB, config.DB.CONNECTION_OPTS);
         console.log("Trying to connect at: " + config.DB.CONNECTION_STRING + config.DB.TRUWORTHS_DB);
+        message = "Successfully connected to the " + store + " database";
         break;
       case 'usave':
         db = mongoose.connect(config.DB.CONNECTION_STRING + config.DB.USAVE_DB, config.DB.CONNECTION_OPTS);
         console.log("Trying to connect at: " + config.DB.CONNECTION_STRING + config.DB.USAVE_DB);
+        message = "Successfully connected to the " + store + " database";
         break;
-      default: break;
+      default:
+        db = mongoose.connect(config.DB.CONNECTION_STRING + config.DB.DEFAULT_DB, config.DB.CONNECTION_OPTS);
+        message = "Successfully connected to the default database"
+      break;
     }
     db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', () => {
-      console.log("Sucessfully connectected to database");
+      console.log(message);
     });
   });
-  return res.json(req.query.q);
-});
+}
 
 app.get('/api/food', (req, res) => {
   const param = req.query.q;
@@ -73,40 +66,38 @@ app.get('/api/food', (req, res) => {
     return;
   }
   else {
-    storeList.map(store => {
-        switch (store) {
-          case 'truworths':
-              console.log('To use the Truworths Model');
-              Product.find({
-                  $text: { $search: param }
-                }, (error, product)  => {
-                if (error) {
-                  console.log(`DB ERROR => ${error}`);
-                }
-                else if(product !== null && typeof product !== undefined ) {
-                  return res.json(product);
-                }
-              });
-              break;
-          case 'usave' :
-            //Use another model here to query
-            console.log('To use the Usave Model');
-            break;
-          default:
-            Product.find({
-                $text: { $search: param }
-              }, (error, product)  => {
-              if (error) {
-                console.log(`DB ERROR => ${error}`);
-              }
-              else if(product !== null && typeof product !== undefined ) {
-                return res.json(product);
-              }
-            });
+    switch (storeList[0]) {
+      case 'truworths':
+        console.log('To use the Truworths Model');
+          Product.find({
+            $text: { $search: param }
+          }, (error, product)  => {
+            if (error) {
+              console.log(`DB ERROR => ${error}`);
+              reconnectToDB(storeList[0]);
+            }
+            else if(product !== null && typeof product !== undefined ) {
+              return res.json(product);
+            }
+          });
           break;
-        }
-    });
-
+      case 'usave' :
+        //Use another model here to query
+        console.log('To use the Usave Model');
+        break;
+      default:
+        Product.find({
+          $text: { $search: param }
+          }, (error, product)  => {
+            if (error) {
+              console.log(`DB ERROR => ${error}`);
+            }
+            else if(product !== null && typeof product !== undefined ) {
+              return res.json(product);
+            }
+        });
+        break;
+      }
     }
 });
 app.listen(app.get('port'), () => {
